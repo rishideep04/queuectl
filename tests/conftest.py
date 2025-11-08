1d2e458971937baf3bce7a09b1fa244794d209b2
Ghanukota Rishi Deep <139058555+rishideep04@users.noreply.github.com>
2025-11-08 10:36:09 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-if ROOT_DIR not in sys.path:
+if ROOT_DIR not in sys.path: 
     sys.path.insert(0, ROOT_DIR)
 
 from db import make_connection,migrate
