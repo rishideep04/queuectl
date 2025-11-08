1d79d811cc45c6d2be385b4aa34db216a340d652
Ghanukota Rishi Deep <139058555+rishideep04@users.noreply.github.com>
2025-11-08 10:34:06 +0530

    Fix import formatting in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if ROOT_DIR not in sys.path:
     sys.path.insert(0, ROOT_DIR)
 
-from db import make_connection, migrate
+from db import make_connection,migrate
 @pytest.fixture(scope="session", autouse=True)
 def setup_database():
     migrate()
