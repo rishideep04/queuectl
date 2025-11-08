b0a53c3265b18b4d9a6ef992b5b076e4cb4ed602
Ghanukota Rishi Deep <139058555+rishideep04@users.noreply.github.com>
2025-11-08 10:35:40 +0530

    Update test_config_mgr.py

--- tests/test_config_mgr.py
+++ tests/test_config_mgr.py
@@ -1,4 +1,4 @@
-from config_mgr import ensure_defaults, get_config, set_config, list_config
+from config_mgr import ensure_defaults,get_config,set_config,list_config
 
 def test_ensure_defaults():
     ensure_defaults()
@@ -7,10 +7,10 @@ def test_ensure_defaults():
     assert "backoff_base" in configs
 
 def test_set_and_get_config():
-    set_config("max_retries", "5")
+    set_config("max_retries","5")
     val = get_config("max_retries")
     assert val == 5
 
 def test_default_values():
-    val = get_config("nonexistent_key", default=42)
+    val = get_config("nonexistent_key",default=42)
     assert val == 42
